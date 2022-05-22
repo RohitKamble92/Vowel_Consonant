ac098162cfee5bf3286597f15665ec3a353033e0
Rohit <rohitkamble10792@gmail.com>
2022-05-22 16:50:31 +0530

    Welcome to Vowel or Consonant program

---
+++ src/com/bridgelabz/vowelorconsonant/VowelOrConsonant.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.vowelorconsonant;
+
+public class VowelOrConsonant {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Vowel or Consonant program");
+    }
+}
